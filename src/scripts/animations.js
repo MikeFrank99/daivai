04358f775bd5f0ebc2457fda94ed0dcb19dabf27
blindblues <blindblues@users.noreply.github.com>
2026-03-17 18:22:56 +0100

    fix(build): GSAP import case-sensitivity and asset resolution in global.css

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -1,6 +1,6 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { Draggable } from "gsap/draggable";
+import { Draggable } from "gsap/Draggable";
 
 gsap.registerPlugin(ScrollTrigger, Draggable);
 
